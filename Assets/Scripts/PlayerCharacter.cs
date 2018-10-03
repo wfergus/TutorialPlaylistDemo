311641a2d7e36698607c1479c229c9c279c51d76
William Fergus <Will.Fergus@iam.local>
2018-10-03 13:19:15 -0500

    adds some of the PlayerCharacter script

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private float horizontalInput;
+
+    private Rigidbody2D PlayerCharacter;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        
+	}
+}
