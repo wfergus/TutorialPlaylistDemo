e7c632becbf6479c9decaef11dca204ee3d92446
William Fergus <Will.Fergus@iam.local>
2018-10-03 14:31:05 -0500

    adds collider jump detection

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -9,23 +9,48 @@ public class PlayerCharacter : MonoBehaviour
 
     [SerializeField]
     private float accelerationForce = 5;
+
     [SerializeField]
     private float jumpForce = 5;
+
     [SerializeField]
     private float maxSpeed = 5;
 
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
     private float horizontalInput;
+    private bool isOnGround;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
     
 
-	void Update ()
+    void Update ()
+    {
+        UpdateIsOnGround();
+        UpdateHorizontalInput();
+        HandleJumpInput();
+    }
+
+    private void UpdateHorizontalInput()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetButtonDown("Jump"))
+    }
+    private void UpdateIsOnGround()
+    {
+       isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+       // Debug.Log("IsOnGround?: " + isOnGround);
+    }
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
             rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-	}
+    }
+
     private void FixedUpdate()
     {
         Move();
