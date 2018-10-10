1ad91dca18ecfcf1684f8bdc43b9afcaa4785a1f
William Fergus <Will.Fergus@iam.local>
2018-10-10 13:53:08 -0500

    adds hazard and scene reload

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered hazard.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            Debug.Log("non-player has entered the hazard");
+        }
+    }
+
+}
